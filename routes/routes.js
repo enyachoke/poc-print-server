7284f4e0be8ad71306d66734dfe54c74af327128
Emmanuel Nyachoke <achachiez@gmail.com>
2016-03-08 14:26:05 +0300

    Move all handlers to there own file added swagger ui

--- routes/routes.js
+++ routes/routes.js
@@ -1,50 +1,32 @@
 'use strict';
 const Joi = require('joi');
-const createLabel = require("../lib/createLabel").createLabel;
-const print = require("../lib/print").print;
-const printer = require('printer');
-let internals = {};
-internals.getPrinters = function(request, reply) {
-  reply(printer.getPrinters());
-};
-internals.printPayload = function(request, reply) {
-  if (request.payload.mergeData) {
-    createLabel({
-        mergeData: request.payload.mergeData,
-        template: request.payload.template
-      })
-      .then(function(buffer) {
-        //We receive a file buffer which we can write to a file or send to printer
-        return print({
-          printerName: request.payload.printer,
-          data: buffer,
-          type: 'PDF'
-        });
-      })
-      .then(function(result) {
-        reply(result);
-      }).catch(function(err) {
-        reply('An error occured');
-      });
-  }
-};
+const handlers = require("../lib/handlers").handlers;
 module.exports = [{
   method: 'POST',
   path: '/print_lab_label',
   config: {
     validate: {
+      options: {
+        allowUnknown: true
+      },
       payload: {
         template: Joi.string(),
         printer: Joi.string(),
-        mergeData: Joi.required()
+        mergeData: Joi.array().items(Joi.object({}))
       }
     },
-    handler: internals.printPayload
+    handler: handlers.printPayload,
+    description: 'Print label',
+    notes: 'This method takes a json payload with printer to be used,template name and an array of merge Data',
+    tags: ['api']
   }
 }, {
   method: 'GET',
   path: '/printers',
   config: {
-    handler: internals.getPrinters
+    handler: handlers.getPrinters,
+    description: 'Get Printers',
+    notes: 'Returns the list of printers available to the local system',
+    tags: ['api']
   }
 }];
